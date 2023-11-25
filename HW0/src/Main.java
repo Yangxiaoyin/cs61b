277328b26991517ad59ad09942b1ecb20a3b2a7c
Chen Yang <chenyang@chens-air.home>
2023-11-25 17:02:22 -0500

    HW0A complete

--- HW0/src/Main.java
+++ HW0/src/Main.java
@@ -13,6 +13,8 @@ public class Main {
 
         }
         printIndexed("ZELDA");
+        stutter("Hello");
+        quadrant(2,4);
         System.out.println("Hello world!");
     }
     public static void printIndexed(String a)
@@ -26,5 +28,30 @@ public class Main {
         System.out.println(str);
 
 
+    }
+
+    public static String stutter(String a)
+    {
+        StringBuilder out= new StringBuilder();
+        for (int i=0;i<=a.length()-1;i++)
+        {
+            out.append(a.charAt(i)).append(a.charAt(i));
+            /*System.out.println(out);*/
+        }
+        System.out.println(out.toString());
+        return out.toString();
+    }
+
+    public static int quadrant(double i, double j)
+    {
+        if (i ==0 || j==0)
+            return 0;
+        else if(i>0 && j >0)
+            return 1;
+        else if(i>0 && j <0)
+            return 4;
+        else if(i<0 && j <0)
+            return 3;
+        else return 2;
     }
 }
